7db68026d1f0dff80ab8e82e3eb84ab76c5702f6
oloko01 <lucas.loureiro1997@gmail.com>
2017-02-11 19:47:10 -0200

    Oi
    Eu n fiz nada

---
+++ Assets/moviment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moviment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	float speed = 2.0f;
+
+	void Update() {
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
