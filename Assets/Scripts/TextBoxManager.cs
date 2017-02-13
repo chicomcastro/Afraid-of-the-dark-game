dc6a5ed82d42091e075a5096fa5c45e00954db22
Felipe Guimarães <Felipe Guimares>
2017-02-12 22:38:48 -0200

    Initial implementation of the dialogue box.

---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public playerMovement player;
+
+    public bool isActive;
+    public bool stopPlayerMov;
+
+    // Use this for initialization
+    void Start() {
+
+        player = FindObjectOfType<playerMovement>();
+
+        if (textFile != null) {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0) {
+            endAtLine = textLines.Length - 1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (stopPlayerMov) {
+            player.canMove = false; //Implementar
+        }
+
+        if (!isActive) return;
+
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown("space")) {
+            currentLine++;
+        }
+
+        if(currentLine == endAtLine) {
+            textBox.SetActive(false);
+            stopPlayerMov = false;
+        }
+    }
+
+    void ShowLines(int _initialLine, int _endLine, bool playerCanMove) {
+        textBox.SetActive(true);
+        stopPlayerMov = !playerCanMove;
+        theText.text = textLines[_initialLine];
+        endAtLine = _endLine;
+    }
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
 	
 	float speed = 2.0f;
-
+    public bool canMove = true;
 	void Update() {
-
+    
 		// Defining moving direction
 		Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 
-		// Moving
-		transform.position += move * speed * Time.deltaTime;
+        // Moving
+        if (canMove) {
+            transform.position += move * speed * Time.deltaTime;
 
-		InvertPlayerSprite (move);
+            InvertPlayerSprite(move);
+        }
 	}
 
 	void InvertPlayerSprite (Vector3 move)
