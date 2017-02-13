3a23b54d4ffa2523fe6eae32193e3c0ce640e0ae
chicomcastro <Franciscommcastro@gmail.com>
2017-02-13 06:28:45 -0200

    Player Movement debugger

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 	
-<<<<<<< HEAD
 	float speed = 3.0f;
 	float delta_time = 0.1f;
 	int i = 1;
 	float time;
-	bool ismoving = false;
-	bool startedMoving = false;
     public bool canMove = true;
 	public Sprite[] animacao;
 	SpriteRenderer spriteRenderer;
@@ -18,24 +15,16 @@ public class playerMovement : MonoBehaviour {
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
 		spriteRenderer.sprite = animacao [0];
 		time = Time.time;
-	}
-=======
-	float speed = 2.0f;
-    public bool canMove = false;
-
-    void Start() {
         canMove = false;
     }
 
 
->>>>>>> 9de340e5bd5deee9fcfd3464cec840b0c7dd7a06
 	void Update() {
         if (!canMove) return;
 		// Defining moving direction
 		Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 
         // Moving
-<<<<<<< HEAD
         if (canMove) {
             transform.position += move * speed * Time.deltaTime;
 			if (move.magnitude != 0) {
@@ -53,12 +42,10 @@ public class playerMovement : MonoBehaviour {
             InvertPlayerSprite(move);
 
         }
-=======
+
         transform.position += move * speed * Time.deltaTime;
 
         InvertPlayerSprite(move);
-        
->>>>>>> 9de340e5bd5deee9fcfd3464cec840b0c7dd7a06
 	}
 		
 	void InvertPlayerSprite (Vector3 move)
